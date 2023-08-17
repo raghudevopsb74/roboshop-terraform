3cf384f0e6d9d2e06b23e8c46292a2769376d05f
Raghu K <raghuk.vit@gmail.com>
2023-08-17 06:18:14 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 module "components" {
-  source   = "git::https://github.com/raghudevopsb74/tf-module-basci-test.git"
-  for_each = var.components
+  source   = "git::https://github.com/raghudevopsb74/tf-module-vpc.git"
 
-  zone_id             = var.zone_id
-  security_groups     = var.security_groups
-  name                = each.value["name"]
-  instance_type       = each.value["instance_type"]
+  for_each = var.vpc
+  cidr = each.value["cidr"]
 }
+
