b4ce0353ee08d7fa6e35f0b03ac2f747cac1c5d3
Raghu K <raghuk.vit@gmail.com>
2023-08-17 07:07:35 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "components" {
-  source   = "git::https://github.com/raghudevopsb74/tf-module-vpc.git"
+  source = "git::https://github.com/raghudevopsb74/tf-module-vpc.git"
 
   for_each = var.vpc
-  cidr = each.value["cidr"]
-  subnets = each.value["subnets"]
+  cidr     = each.value["cidr"]
+  subnets  = each.value["subnets"]
 
 }
 
