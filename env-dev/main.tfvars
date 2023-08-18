8a6142180985508a4894ecb2bf8a651891ebcfaf
Raghu K <raghuk.vit@gmail.com>
2023-08-18 07:07:27 +0530

    Terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -19,4 +19,6 @@ vpc = {
 }
 
 
-default_vpc_id = "vpc-0a8e3e6a0d6cb90d9"
+default_vpc_id             = "vpc-0a8e3e6a0d6cb90d9"
+default_vpc_cidr           = "172.31.0.0/16"
+default_vpc_route_table_id = "rtb-0b9a867762d6530ba"
--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 module "vpc" {
   source = "git::https://github.com/raghudevopsb74/tf-module-vpc.git"
 
-  for_each       = var.vpc
-  cidr           = each.value["cidr"]
-  subnets        = each.value["subnets"]
-  default_vpc_id = var.default_vpc_id
-
+  for_each                   = var.vpc
+  cidr                       = each.value["cidr"]
+  subnets                    = each.value["subnets"]
+  default_vpc_id             = var.default_vpc_id
+  default_vpc_cidr           = var.default_vpc_cidr
+  default_vpc_route_table_id = var.default_vpc_route_table_id
 }
 
 output "vpc" {
