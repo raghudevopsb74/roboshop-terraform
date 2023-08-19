bc14dbc4a445aa30be73f1ee58a6dae0b4d57378
Raghu K <raghuk.vit@gmail.com>
2023-08-19 07:06:31 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -11,7 +11,20 @@ module "vpc" {
   env                        = var.env
 }
 
-output "vpc" {
-  value = module.vpc
-}
+#module "alb" {
+#  source = "git::https://github.com/raghudevopsb74/tf-module-alb.git"
+#
+#  for_each        = var.alb
+#  internal        = each.value["internal"]
+#  lb_type         = each.value["lb_type"]
+#  sg_ingress_cidr = each.value["sg_ingress_cidr"]
+#  vpc_id          = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+#  subnets         = each.value["internal"] ?  : ""
+#  tags            = var.tags
+#  env             = var.env
+#  sg_port         = each.value["sg_port"]
+#}
+
+
+
 
