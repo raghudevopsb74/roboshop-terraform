e77bfdc229d795ae6daf3d2c839f342779235dfe
Raghu K <raghuk.vit@gmail.com>
2023-08-22 07:15:19 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -30,9 +30,13 @@ module "docdb" {
   tags   = var.tags
   env    = var.env
 
-  for_each   = var.docdb
-  subnet_ids = local.db_subnets
-
+  for_each                = var.docdb
+  subnet_ids              = local.db_subnets
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  vpc_id                  = vpc_id
+  sg_ingress_cidr         = local.app_subnets_cidr
 }
 
 
