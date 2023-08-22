cadbdf058a9cc661ec1d57d506bbada092dbbfd1
Raghu K <raghuk.vit@gmail.com>
2023-08-22 07:23:57 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -37,7 +37,10 @@ module "docdb" {
   skip_final_snapshot     = each.value["skip_final_snapshot"]
   vpc_id                  = local.vpc_id
   sg_ingress_cidr         = local.app_subnets_cidr
+  engine_version          = each.value["engine_version"]
 }
+
+
 
 
 
