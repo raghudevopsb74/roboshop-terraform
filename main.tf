86e9dae9b95f59cbccd54487287a912c7b0a72ea
Raghu K <raghuk.vit@gmail.com>
2023-08-30 06:18:43 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -100,7 +100,8 @@ module "rabbitmq" {
 }
 
 module "app" {
-  source = "git::https://github.com/raghudevopsb74/tf-module-app.git"
+  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
+  source     = "git::https://github.com/raghudevopsb74/tf-module-app.git"
 
   tags             = var.tags
   env              = var.env
