38712e127fc3d5fa9ec197c8ef85f09afbe79cb6
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-22 06:35:43 +0530

    Changes

--- main.tf
+++ main.tf
@@ -173,11 +173,10 @@ module "eks" {
 
 
   eks_managed_node_groups = {
-    blue = {}
     green = {
-      min_size     = 1
+      min_size     = 3
       max_size     = 10
-      desired_size = 1
+      desired_size = 3
 
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
